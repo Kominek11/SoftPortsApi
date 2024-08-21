3f83c423d23749df447ac43113d602db3e9aea33
Lucca Kominek <91330835+Kominek11@users.noreply.github.com>
2024-08-21 17:09:51 -0300

    att

--- Dockerfile
+++ Dockerfile
@@ -11,7 +11,7 @@ ENV KEYCLOAK_ADMIN_PASSWORD=admin
 COPY keycloak-25.0.2 /opt/keycloak
 
 # Expose the default Keycloak port
-EXPOSE 8080
+EXPOSE 8090
 
 # Start Keycloak
 ENTRYPOINT ["/opt/keycloak/bin/kc.sh", "start-dev"]
--- core/src/main/java/br/com/softports/core/api/organizacao/usecase/BuscarOrganizacoes.java
+++ core/src/main/java/br/com/softports/core/api/organizacao/usecase/BuscarOrganizacoes.java
@@ -2,7 +2,6 @@ package br.com.softports.core.api.organizacao.usecase;
 
 import br.com.softports.core.api.common.dto.Pagina;
 import br.com.softports.core.api.organizacao.dto.OrganizacaoResponse;
-import br.com.softports.core.api.regra.dto.RegraResponse;
 
 public interface BuscarOrganizacoes {
 
